4ba48f51b382879dc1af5ca11ce8439cbc4d94e5
odran037 <leonardo.prates@outlook.com>
2017-11-18 18:13:52 -0600

    SPA routing

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -4,28 +4,32 @@ $(document).ready(function() {
    /// Handle navigation
   /////////////////////////
 
+
   // Load front page
   const mainHeader = '<img src="/assets/company-logos/gdt-logo.png" alt="GDT Logo"> Agile Operations';
   $('#main').load(`/partials/Home.html`);
   $('#page-title').html(mainHeader);
 
+
   // Generate navigation bar
   projects.forEach(project => {
     $('#navbar').append(`<li class="nav-item">
-      <a class="nav-link" href="#" data-template="${project.label}">${project.label}</a>
+      <a class="nav-link" href="#${project.label}" data-template="${project.label}">${project.label}</a>
     </li>`);
-  })
-
-  // Load each partial into main page
-  $('[data-template]').click(function(e) {
-    e.preventDefault();
-    let partial = $(this).data('template');
-    if (partial === 'Home') {
-      $('#page-title').html(mainHeader);
-    } else {
-      $('#page-title').text(partial);
-    }
-    $('#main').load(`/partials/${partial}.html`);
   });
 
+  // SPA Routing allow browser back and forward
+  $(window).on('hashchange load', function() {
+    if (window.location.hash) {
+      var partial = window.location.hash.replace('#', '');
+      if (partial === 'Home') {
+        $('#page-title').html(mainHeader);
+      } else {
+        $('#page-title').text(partial);
+      }
+      $('#main').load(`/partials/${partial}.html`);
+    } else {
+      $('#main').load(`/partials/home.html`);
+    }
+  });
 });
