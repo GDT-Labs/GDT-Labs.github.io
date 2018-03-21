b5321135c7966085599f7e27231675d4f7c2a2cb
odran037 <leonardo.prates@outlook.com>
2018-03-21 15:36:02 -0500

    new projects data and layout

--- partials/data/projects.js
+++ partials/data/projects.js
@@ -59,5 +59,51 @@ const projects = [
         description: "Stealth project involving deploying and provisioning OEM switches with proper images, base configs and business logic."
       }
     ]
+  },
+  {
+    label: "Presales",
+    data: [
+      {
+        title: "North Central Texas Council of Governments (NTCTOG",
+        description: "Is a data warehouse project to update their SQL Server data systems, involving ETL of many disparate data sources into one data system which can ingest these various sources and supply data for BI projects in MS Power BI as well as supporting NCTOG's ongoing 911 and e911 services. It includes data sources such as call center data, CDRs (Call Data Records), geodata, and reporting."
+      },
+    ]
+  },
+  {
+    label: "External",
+    data: [
+      {
+        title: "SES",
+        description: "GDT to provide back-end services for their Network Observer Portal(NOP). The NOP enables users to view metrics data that has been collected about various networked devices. APIs are provided to the NOP front end allowing this data to be retrieved, queried and aggregated, with the results being presented as either raw time series data or in a format ready to be presented as a number of different types of graphs."
+      },
+      {
+          title: "T-Mobile",
+          description: "<strong>EPIF</strong>: Build a solution that will give engineers electronic access to a information store where work orders describing the job are accessible and able to be modified on the fly. Full audit controls allow for forensic tracking of all document changes.<br><br><strong>Port decommission</strong>: Build a solution to allow the operational teams at T-Mobile to automate port decommissioning on network devices. We propose to automate the process in order to reduce errors, shorten the time to delivery and lessen the burden on all involved teams."
+      },
+      {
+          title: "Myrtle consulting (Kellogs -Data Aggregation)",
+          description: "GDT will help evolve clients IIoT infrastructure to capture, monitor, analyze and take action on many of their manufacturing lines.  Data aggregation from various sensor sources and state-of-the-art infrastructure upgrades will drive efficiency and accuracy on the production line."
+      },
+      {
+          title: "Monsaton Integration",
+          description: "Monsanto is one of GDT’s key clients they provide managed services for.  Monsanto uses BMC Remedy as their ticketing system.  They would like to integrate their system with GDT’s ServiceNow instance to enable cross-company ticket sharing and joint ticket resolution."
+      },
+      {
+          title: "Loomis",
+          description: "GDT has created a modular system for a connected “Truck of the Future” that combines secure transport, fleet tracking, remote monitoring, and vehicle diagnostics into a single pane of glass using technologies like 4G, RFID, GPS, and OBDII."
+      },
+      {
+          title: "Spark Bridge",
+          description: "Integration of Spark into CILC environment to include: Virtual tour via Spark space, Professional Learning Network to select topics related to interest and grade level, and Hot Topic Notifications to increase participation in Spark Professional Learning Network for popular spaces."
+      },
+      {
+          title: "NSO",
+          description: "Opportunity to develop a NSO management portal for Internet2 and Universities across the country.  Sponsored by Cisco, GDT will install and connect NSO platforms at various Universities and build a cloud portal to allow service chaining between nodes.  This will allow researches to share infrastructure across University boundaries by building topologies through the campus network and into Internet2."
+      },
+      {
+          title: "US Cellular",
+          description: "Customer is looking for best practices and assessments to help derive value from common tasks.  Particularly, they have identified their SOX compliance and Cisco USC patching processes as being potential candidates for additional automation.  GDT will engage to examine processes and make recommendations on improvements via tooling, automation or workflow changes."
+      }
+    ]
   }
 ];
